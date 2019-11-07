333a235de8a5240a87e1002d56b4cde45392072b
fernvnat14 <32473192+fernvnat14@users.noreply.github.com>
2019-11-07 14:15:05 +0700

    first commit

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMudsPgszocf0bxP8z33jgDdARI8_Ee6A",
+  authDomain: "petid-vue-project-5e649.firebaseapp.com",
+  databaseURL: "https://petid-vue-project-5e649.firebaseio.com",
+  projectId: "petid-vue-project-5e649",
+  storageBucket: "petid-vue-project-5e649.appspot.com",
+  messagingSenderId: "763800915255",
+  appId: "1:763800915255:web:1bb922c2b48b231f3e9eb2",
+  measurementId: "G-SV19LPZSHW"
+};
